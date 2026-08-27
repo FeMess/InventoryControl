3e6fc186f4663126d5ec5f9a0b9aec42c7c42f37
FeMess <=>
2026-08-27 20:14:45 -0300

    refactor: adding status to the class

--- models/product.py
+++ models/product.py
@@ -1,6 +1,7 @@
 class Product:
-    def __init__(self, identifier, name: str, price: float):
-        self.ID = identifier
+    def __init__(self, identifier: int, name: str, price: float):
+        self.id = identifier
         self.name = name
         self.price = price
         self.quantity = 0
+        self.status = "active"
