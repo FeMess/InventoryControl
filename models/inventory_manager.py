3c85e102fa8b9e9e79c5f9d2e596d29222c83b24
FeMess <=>
2026-08-27 20:12:22 -0300

    feature: adding feat to save history data

--- models/inventory_manager.py
+++ models/inventory_manager.py
@@ -1,9 +1,13 @@
+from datetime import datetime
+
+from models.movement import Movement
 from models.product import Product
 
 
 class InventoryManager:
     def __init__(self):
         self.products = []
+        self.history = []
 
     # Public Methods
 
@@ -11,12 +15,10 @@ class InventoryManager:
         self._validate_name(name)
         self._validate_price(price)
 
-        new_identifier = self._find_new_identifier()
         name = name.strip().title()
+        new_product = Product(self._find_new_identifier(self.products), name, price)
 
-        new_product = Product(new_identifier, name, price)
         self.products.append(new_product)
-
         return new_product
 
     def list_products(self):
@@ -26,7 +28,7 @@ class InventoryManager:
         self._validate_identifier(identifier)
 
         for product in self.products:
-            if product.ID == identifier:
+            if product.id == identifier:
                 return product
 
         raise ValueError("The product ID does not exist.")
@@ -41,10 +43,10 @@ class InventoryManager:
         product.name = name
         product.price = price
 
-    def remove_product(self, identifier):
+    def deactivate_product(self, identifier):
         product = self.view_product(identifier)
 
-        self.products.remove(product)
+        product.status = "inactive"
 
     def register_entry(self, identifier, quantity):
         self._validate_quantity(quantity)
@@ -52,6 +54,9 @@ class InventoryManager:
         product = self.view_product(identifier)
 
         product.quantity += quantity
+
+        # Saving ENTRY Movement
+        self._register_movement(product, "ENTRY", quantity)
 
     def register_exit(self, identifier, quantity):
         self._validate_quantity(quantity)
@@ -63,14 +68,28 @@ class InventoryManager:
 
         product.quantity -= quantity
 
+        # Saving EXIT Movement
+        self._register_movement(product, "EXIT", quantity)
+
     # Internal Methods
-    def _find_new_identifier(self):
-        biggest_ID = 0
+    def _register_movement(self, product, movement_type, quantity):
+        new_movement = Movement(
+            self._find_new_identifier(self.history),
+            datetime.now(),  # noqa: DTZ005
+            product.id,
+            movement_type,
+            quantity,
+        )
 
-        for product in self.products:
-            biggest_ID = max(biggest_ID, product.ID)
+        self.history.append(new_movement)
 
-        return biggest_ID + 1
+    def _find_new_identifier(self, list_objects):
+        biggest_id = 0
+
+        for item in list_objects:
+            biggest_id = max(biggest_id, item.id)
+
+        return biggest_id + 1
 
     def _validate_identifier(self, identifier):
         if not isinstance(identifier, int):
---
+++ models/movement.py
@@ -0,0 +1,14 @@
+class Movement:
+    def __init__(
+        self,
+        identifier: int,
+        created_on,
+        product_id: int,
+        movement_type: str,
+        quantity: int | float,  # noqa: PYI041
+    ):
+        self.id = identifier
+        self.created_on = created_on
+        self.product_id = product_id
+        self.moviment_type = movement_type
+        self.quantity = quantity
